d09b7556f0706056379ab3c8dfa29defd3f88e86
azizdjan_official <tulaganovazizjon@gmail.com>
2023-10-31 07:46:59 +0500

    homework is done

---
+++ app.js
@@ -0,0 +1,32 @@
+
+
+// function lbsChecker(son){
+//     let lbs1 = 0.453592;
+    
+//     console.log(Number(son) * lbs1 + " kg");
+// }
+// lbsChecker(10)
+
+// 1chi vazifa javobi
+
+
+// function BMIChecker(height, weight){
+    
+//     console.log(Number(height) * Number(height) / Number(weight));    
+// }
+// BMIChecker(1.70,68);
+
+// 2chi vazifa javobi
+
+// let oylanganSon = prompt("Biror son o'ylang")
+// let randomSon = Math.floor(Math.random() * 10) + 1;
+
+
+//     if(oylanganSon ==randomSon)
+//     {
+//         alert('Oylagan' + randomSon + 'iz togri chiqdi')
+//     }else{
+//         alert('afsuski men ' +randomSon+ " sonini o'ylagandim")
+//     }    
+
+// 3chi vazifa javobi
